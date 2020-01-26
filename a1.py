591e2f4e4cab112be160f27a70e2eca8bf1b38be
Rose Dufresne <32066859+RoseDuf@users.noreply.github.com>
2020-01-25 21:33:38 -0500

    added timed lyrics

--- a1.py
+++ a1.py
@@ -37,7 +37,8 @@ def parseFile(file):
             
         if (count == 2):
             array = line.split(',')
-            lyric.startingTime = array[0]
+            lyric.startingTime = convertToSeconds(array[0])
+            
             #lyric.EndingTime = array[1]
             #print("time: ", lyric.startingTime, "\n")
              
@@ -53,17 +54,48 @@ def parseFile(file):
             count = 0
             listIndex += 1
             lyricList.append(lyric)
+            
+            
+def convertToSeconds(time):
+    seconds = 0
+    seconds += int(time[6:8])
+    seconds += int(time[3:5])*60
+    seconds += int(time[0:2])*3600
+    
+    #print(seconds)
+    
+    return seconds
         
 def findTimeStamp(time):
     
     #print(lyricList[0].startingTime)
+    newTime = convertToSeconds(time)
     
     for i,_ in enumerate(lyricList):
-        if (lyricList[i].startingTime == time):
+        if (lyricList[i].startingTime == newTime):
             print(lyricList[i].sentence)
-            
-   
-    
-parseFile('bj.txt')
-findTimeStamp("00:00:34")
 
+def displayLyricsTimer():
+    
+    parseFile('bj.txt')
+    
+    starttime=time.time()
+
+    timeStampFound = False
+    index = 0
+    targetTime = 0
+
+    while True:
+        time.sleep(1 - ((time.time() - starttime) % 1))
+        if (timeStampFound == False):
+            targetTime = lyricList[index].startingTime
+            timeStampFound = True
+
+        if (int(time.time() - starttime) == targetTime):
+            print(lyricList[index].sentence)
+            index += 1
+            timeStampFound = False
+
+        print(int(time.time() - starttime))
+        
+displayLyricsTimer()
