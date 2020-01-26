694557585737a5a2c52941c7c6ca04ab3505a099
JAdamWeller <Jadamweller@gmail.com>
2020-01-25 23:53:17 -0500

    Add files via upload

--- songSearch.py
+++ songSearch.py
@@ -17,14 +17,14 @@ params = (
 response = requests.get('https://conuhacks-2020.tsp.cld.touchtunes.com/v1/songs', headers=headers, params=params).json()
 try:
     print(response)
-    with open(str(songName1) + '.json', 'w') as outfile:
+    with open(str('theFile') + '.json', 'w') as outfile:
         json.dump(response, outfile)
 except:
     print("Doesn't work")
 
 songList = []
 songIdList = []
-with open(str(songName1)+'.json') as f:
+with open('theFile'+'.json') as f:
     data = json.load(f)
 for i in range(len(data["songs"])):
     if data["songs"][i]["id"] not in songIdList:
@@ -45,13 +45,13 @@ response = requests.get('https://conuhacks-2020.tsp.cld.touchtunes.com/v1/songs/
                             headers=headers).json()
 try:
     print(response)
-    with open(str(songName1)+ 'Id.json', 'w') as outfile:
+    with open('theFile'+ 'Id.json', 'w') as outfile:
         json.dump(response, outfile)
 except:
         print("Doesn't work")
 
 
-with open(str(songName1)+ 'Id.json') as f:
+with open(str('theFile')+ 'Id.json') as f:
     data = json.load(f)
 
     print(data["artist"]["name"])
@@ -60,13 +60,15 @@ with open(str(songName1)+ 'Id.json') as f:
     print(data["album"]["title"])
 
 url = str(data["playUrl"])
+print(url)
 r = requests.get(url)
 
 notNeeded = 'https://cdn.apps.playnetwork.com/master/'
 myfile = requests.get(url)
 fileName = url[:url.rindex('.ogg')+4]
 fileName = fileName.replace(str(notNeeded), "")
+theFile = 'theFile.ogg'
 try:
-    open(fileName, 'wb').write(myfile.content)
+    open(theFile, 'wb').write(myfile.content)
 except:
     print("File already exists")
